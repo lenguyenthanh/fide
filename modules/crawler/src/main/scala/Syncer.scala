2ab0cd0ba24649fa8555ae3395bb458a266febc6
Thanh Le <lenguyenthanh@hotmail.com>
2024-04-12 22:03:53 +0700

    Refactor Crawler module
    Now We only save the last update when finished crawling

---
+++ modules/crawler/src/main/scala/Syncer.scala
@@ -0,0 +1,44 @@
+package fide
+package crawler
+
+import cats.effect.IO
+import cats.syntax.all.*
+import fide.db.KVStore
+import org.http4s.*
+import org.http4s.client.Client
+import org.typelevel.log4cats.Logger
+import org.typelevel.log4cats.syntax.*
+
+trait Syncer:
+  def fetchStatus: IO[Syncer.Status]
+  def saveLastUpdate(value: String): IO[Unit]
+
+object Syncer:
+  val fideLastUpdateKey = "fide_last_update_key"
+
+  enum Status:
+    case UpToDate
+    case OutDated(timestamp: Option[String])
+
+  object Status:
+    def apply(local: Option[String], remote: Option[String]): Status =
+      (local, remote).match
+        case (Some(l), Some(r)) if l == r => Status.UpToDate
+        case (Some(l), Some(r))           => Status.OutDated(r.some)
+        case (None, Some(r))              => Status.OutDated(r.some)
+        case (_, None)                    => Status.OutDated(none)
+
+  def instance(store: KVStore, client: Client[IO])(using Logger[IO]): Syncer =
+    Syncer(store, FideScraper(client))
+
+  def apply(store: KVStore, scraper: FideScraper)(using Logger[IO]): Syncer = new:
+    def fetchStatus: IO[Status] =
+      (lastLocalUpdate, scraper.lastUpdate).flatMapN: (local, remote) =>
+        info"last local update: $local, last remote update: $remote" *>
+          IO(Status(local, remote))
+
+    private def lastLocalUpdate: IO[Option[String]] =
+      store.get(fideLastUpdateKey)
+
+    def saveLastUpdate(value: String): IO[Unit] =
+      store.put(fideLastUpdateKey, value) *> info"Saved last update date: $value"
