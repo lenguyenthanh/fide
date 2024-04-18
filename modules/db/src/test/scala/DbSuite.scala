cfbc04e00a59b0f9eadc0183cf768f1c73c624f4
Thanh Le <lenguyenthanh@hotmail.com>
2024-04-18 22:59:02 +0700

    Add Crawler test

--- modules/db/src/test/scala/DbSuite.scala
+++ modules/db/src/test/scala/DbSuite.scala
@@ -23,7 +23,7 @@ object DbSuite extends SimpleIOSuite:
     "John",
     Title.GM.some,
     Title.WGM.some,
-    List(OtherTitle.FST),
+    List(OtherTitle.FI, OtherTitle.LSI),
     2700.some,
     2700.some,
     2700.some,
@@ -44,9 +44,8 @@ object DbSuite extends SimpleIOSuite:
   test("create players success"):
     val player2 = newPlayer.copy(name = "Jane", id = 2)
     resource
-      .use(
+      .use:
         _.upsert(List(newPlayer -> newFederation.some, player2 -> newFederation.some)).map(_ => expect(true))
-      )
 
   test("create and query player success"):
     resource.use: db =>
