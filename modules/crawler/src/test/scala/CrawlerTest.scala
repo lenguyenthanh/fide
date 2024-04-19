088fb1755fda81a3c31f4d05b801bbafedb81322
Thanh Le <lenguyenthanh@hotmail.com>
2024-04-19 08:52:20 +0700

    Make crawler aware of FiDE woman inactive
    Apparently, FIDE differentiates between inactive & women inactive

--- modules/crawler/src/test/scala/CrawlerTest.scala
+++ modules/crawler/src/test/scala/CrawlerTest.scala
@@ -7,10 +7,18 @@ import weaver.*
 
 object CrawlerTest extends FunSuite:
 
-  test("create player success"):
+  test("player with other titles"):
     val x = Downloader
       .parse:
         "10001492       Ojok, Patrick                                                UGA M             FI,LSI             1638  0   20 1932  0   20 1926  0   20 1974      "
       .get
       ._1
     expect(x.otherTitles == List(OtherTitle.FI, OtherTitle.LSI))
+
+  test("inactive player"):
+    val x = Downloader
+      .parse:
+        "8605360        A La, Teng Hua                                               CHN F                                1949  0   40                           1993  wi  "
+      .get
+      ._1
+    expect(!x.active)
