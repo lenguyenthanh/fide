b592f9f60d5a2993f70e5c03b47262dccc261916
Thanh Le <lenguyenthanh@hotmail.com>
2024-04-25 12:32:05 +0700

    Add warmup and stress scenario

--- modules/gatling/src/test/scala/gatling/SimpleSimulation.scala
+++ modules/gatling/src/test/scala/gatling/SimpleSimulation.scala
@@ -23,16 +23,19 @@ class SimpleSimulation extends Simulation:
   def federationSummary(page: Int): String =
     s"/federations/summary?page=$page"
 
-  def requests(name: String, f: Int => String) =
-    Range.inclusive(1, config.maxPages).map(f).map(http(name).get(_).check(status.is(200)))
+  def requests(page: Int, name: String, f: Int => String) =
+    Range.inclusive(1, page).map(f).map(http(name).get(_).check(status.is(200)))
 
 // Add the ScenarioBuilder:
-  val one = scenario("Simple Scenario")
-    .exec(requests("default-players", defaultPlayers))
-    .exec(requests("standard-players", sortByStandard))
-    .exec(requests("federation-summary", federationSummary))
+  def makeScenario(page: Int, name: String) = scenario(name)
+    .exec(requests(page, "default-players", defaultPlayers))
+    .exec(requests(page, "standard-players", sortByStandard))
+    .exec(requests(page, "federation-summary", federationSummary))
 
-  setUp(one.inject(config.injectionPolicy)).protocols(httpProtocol)
+  val warmup = makeScenario(1, "warmup").inject(config.warmupPolicy)
+  val stress = makeScenario(config.maxPages, "stress").inject(config.stressPolicy)
+
+  setUp(warmup.andThen(stress)).protocols(httpProtocol)
 
 object SimpleSimulation:
   private val hist = new ConcurrentHistogram(1L, 10000L, 3)
@@ -45,8 +48,10 @@ object SimpleSimulation:
 
   object config:
     val numberOfUsers = 1000
+    val warmupUsers   = 100
     val maxPages      = 10
 
     val serverUri = "http://localhost:9669/api"
 
-    val injectionPolicy: OpenInjectionStep = rampUsers(numberOfUsers).during(30.seconds)
+    val warmupPolicy: OpenInjectionStep = rampUsers(warmupUsers).during(30.seconds)
+    val stressPolicy: OpenInjectionStep = rampUsers(numberOfUsers).during(30.seconds)
