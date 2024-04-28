8ba7f6f33f746a62d5848bb45474407cf1b7ced3
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2024-04-27 23:08:21 -0400

    Get lastUpdate from response header

--- modules/crawler/src/main/scala/FideScraper.scala
+++ modules/crawler/src/main/scala/FideScraper.scala
@@ -1,43 +1,33 @@
 package fide.crawler
 
 import cats.effect.IO
-import fs2.*
 import org.http4s.*
 import org.http4s.client.Client
 import org.http4s.implicits.*
+import org.typelevel.ci.CIString
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.syntax.*
 
 trait FideScraper:
   def lastUpdate: IO[Option[String]]
 
-// We need to find this line and extract the date "02 Apr 2024"
-// <i class="fa   fa-download" style="color: #50618d; padding-right: 10px;"></i> <a href=http://ratings.fide.com/download/players_list.zip class=tur>TXT format</a> <small>(02 Apr 2024, Sz: 30.83 MB)</small> <br><br>
 object FideScraper:
-  // TODO: this is a terrible implementation, should probably use regex to get the date
   def apply(client: Client[IO])(using Logger[IO]): FideScraper = new:
 
     def lastUpdate: IO[Option[String]] =
       client
         .run(request)
-        .use(extract)
+        .use(extract.andThen(IO.pure))
         .handleErrorWith(e => error"Failed to get last update date: $e".as(None))
 
-  private val preDateString =
-    """<i class="fa   fa-download" style="color: #50618d; padding-right: 10px;"></i> <a href=http://ratings.fide.com/download/players_list.zip class=tur>TXT format</a> <small>("""
-
-  private val downloadUrl = "http://ratings.fide.com/download/players_list.zip"
-
   private val request = Request[IO](
-    method = Method.GET,
-    uri = uri"https://ratings.fide.com/download_lists.phtml"
+    method = Method.HEAD,
+    uri = uri"http://ratings.fide.com/download/players_list.zip"
   )
 
-  private val extract: Response[IO] => IO[Option[String]] =
-    _.bodyText
-      .map(_.linesIterator.filter(_.contains(downloadUrl)))
-      .filter(_.nonEmpty)
-      .map(_.next) // WARNING: This is fine only because this is placed after filter(_.nonEmpty)
-      .map(_.drop(preDateString.length).takeWhile(_ != ','))
-      .compile
-      .last
+  private val extract: Response[IO] => Option[String] = response =>
+    response.headers
+      .get(CIString("Last-Modified"))
+      .map { header =>
+        header.head.value
+      }
