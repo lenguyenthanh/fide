de977a41f85e51c331c9dc127cacc624fae6d137
Thanh Le <lenguyenthanh@hotmail.com>
2024-05-11 20:06:21 +0700

    Retire heroku deployment

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: modules/backend/target/universal/stage/bin/backend -Dhttp.port=${PORT}  -Dpostgres.ssl=true -Dpostgres.max=40 -Dpostgres.schema=fide -Dhttp.shutdown.timeout=1 -Dcrawler.chunkSize=100 -Dcrawler.concurrent.upsert=10
--- modules/backend/src/main/scala/app.config.scala
+++ modules/backend/src/main/scala/app.config.scala
@@ -47,6 +47,12 @@ object CrawlerConfig:
   def config = (chunkSize, concurrentUpsert).parMapN(crawler.CrawlerConfig.apply)
 
 object PostgresConfig:
+
+  private def host     = env("POSTGRES_HOST").or(prop("postgres.host")).as[Host].default(ip"0.0.0.0")
+  private def port     = env("POSTGRES_PORT").or(prop("postgres.port")).as[Port].default(port"5432")
+  private def user     = env("POSTGRES_USER").or(prop("postgres.user")).as[String]
+  private def password = env("POSTGRES_PASSWORD").or(prop("postgres.password")).as[String]
+  private def database = env("POSTGRES_DATABASE").or(prop("postgres.database")).as[String]
   // max concurrent sessions
   private def max    = env("POSTGRES_MAX").or(prop("postgres.max")).as[Int]
   private def schema = env("POSTGRES_SCHEMA").or(prop("postgres.schema")).as[String].default("fide")
@@ -54,36 +60,14 @@ object PostgresConfig:
   private def ssl    = env("POSTGRES_SSL").or(prop("postgres.ssl")).as[Boolean].default(false)
 
   def config =
-    (PostgresDriverLoader.loadDriver, max, schema, debug, ssl).parMapN:
-      case ((host, port, user, password, database), max, schema, debug, ssl) =>
-        db.PostgresConfig(host, port, user, password, database, max, schema, debug, ssl)
-
-type DriverUrl = (Host, Port, String, String, String)
-
-object PostgresDriverLoader:
-  private def url =
-    env("DATABASE_URL")
-      .or(prop("database.url"))
-      .as[DriverUrl]
-
-  private def host     = env("POSTGRES_HOST").or(prop("postgres.host")).as[Host].default(ip"0.0.0.0")
-  private def port     = env("POSTGRES_PORT").or(prop("postgres.port")).as[Port].default(port"5432")
-  private def user     = env("POSTGRES_USER").or(prop("postgres.user")).as[String]
-  private def password = env("POSTGRES_PASSWORD").or(prop("postgres.password")).as[String]
-  private def database = env("POSTGRES_DATABASE").or(prop("postgres.database")).as[String]
-  val loadDriver       = url.or((host, port, user, password, database).tupled)
-
-  given ConfigDecoder[String, DriverUrl] =
-    ConfigDecoder.instance[String, DriverUrl]((_, url) => decode(url))
-
-  def decode(url: String): Either[ConfigError, DriverUrl] =
-    (for
-      parsed   <- Either.catchNonFatal(java.net.URI(url)).toOption
-      host     <- Host.fromString(parsed.getHost())
-      port     <- Port.fromInt(parsed.getPort())
-      userInfo <- Option(parsed.getUserInfo())
-      dbName   <- Option(parsed.getPath().tail) // dropping the first slash
-      userName <- Option(userInfo.split(":").apply(0))
-      password <- Option(userInfo.split(":").apply(1))
-    yield (host, port, userName, password, dbName))
-      .toRight(ConfigError(s"Failed to parse DATABASE_URL $url"))
+    (
+      host,
+      port,
+      user,
+      password,
+      database,
+      max,
+      schema,
+      debug,
+      ssl
+    ).parMapN(db.PostgresConfig.apply)
