551aa7e788e7c485bb66f83783eed6d4f3e9549c
Thanh Le <lenguyenthanh@hotmail.com>
2025-01-31 16:01:08 +0100

    Add warning log when crawler finds new federations

--- modules/crawler/src/test/scala/CrawlerTest.scala
+++ modules/crawler/src/test/scala/CrawlerTest.scala
@@ -2,18 +2,21 @@ package fide
 package crawler
 package test
 
+import cats.effect.IO
 import cats.syntax.all.*
-import fide.domain.{ NewPlayer, OtherTitle }
+import fide.domain.OtherTitle
+import org.typelevel.log4cats.Logger
 import weaver.*
 
-object CrawlerTest extends FunSuite:
+object CrawlerTest extends SimpleIOSuite:
+
+  given Logger[IO] = org.typelevel.log4cats.noop.NoOpLogger[IO]
 
   test("player with other titles"):
-    val x =
-      parse:
-        "10001492       Ojok, Patrick                                                UGA M             FI,LSI             1638  0   20 1932  0   20 1926  0   20 1974      "
-      .get
-    expect(x.otherTitles == List(OtherTitle.FI, OtherTitle.LSI))
+    parse(
+      "10001492       Ojok, Patrick                                                UGA M             FI,LSI             1638  0   20 1932  0   20 1926  0   20 1974      "
+    ).map(_.get.otherTitles)
+      .map(x => expect(x == List(OtherTitle.FI, OtherTitle.LSI)))
 
   test("active flag"):
     val lines = List(
@@ -22,7 +25,9 @@ object CrawlerTest extends FunSuite:
       "29976634       Aafrin, S F Aja                                              SRI F                                                                       2012  w   ",
       "1478800        Aagaard, Christian                                           DEN M                                                                       1999      "
     )
-    val actives = lines.traverse(parse).get.map(_.active)
-    expect(actives == List(false, false, false, true))
+    lines
+      .traverse(parse)
+      .map(_.flatten.map(_.active))
+      .map(x => expect(x == List(false, false, false, true)))
 
-  private def parse(s: String): Option[NewPlayer] = Downloader.parse(s).map(_._1)
+  private def parse(s: String) = Downloader.parse(s).map(_.map(_._1))
