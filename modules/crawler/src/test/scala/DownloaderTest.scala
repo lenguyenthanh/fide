aadf08a509502965c7349296f0ae2001d43cdc5b
Thanh Le <lenguyenthanh@hotmail.com>
2025-02-01 14:34:05 +0100

    Ignore DownloaderTest suite
    As we already test it in DbIntegrationSuite

--- modules/crawler/src/test/scala/DownloaderTest.scala
+++ modules/crawler/src/test/scala/DownloaderTest.scala
@@ -13,7 +13,7 @@ object DownloaderTest extends SimpleIOSuite:
 
   given Logger[IO] = org.typelevel.log4cats.noop.NoOpLogger[IO]
 
-  test("No new federations after downloads"):
+  test("No new federations after downloads".ignore):
     EmberClientBuilder
       .default[IO]
       .build
